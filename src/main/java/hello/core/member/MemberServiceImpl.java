4be41e6273cc8f8d91ebb8483a766fd21a5d7c8b
김민정 <>
2021-06-21 20:45:29 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    //    interface만 가지면 nullpointException 뜨겠 ?
+//    이때 구현할 객체를 선택해줘야함 -> MemoryMemberRepository
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
