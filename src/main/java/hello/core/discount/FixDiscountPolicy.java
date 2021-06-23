3e27e691bb68edd9a02174002b95398a751e90fe
김민정 <>
2021-06-23 21:47:07 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;  //1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+
+        // enum은 원래 ==로 사용하는거 맞음
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+
+    }
+}
---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,65 @@
+package hello.core.order;
+
+
+// 할인 다 끝나고 만들어지는 객체라고 생각하면 될듯
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice; // 원가
+    private int discountPrice;  // 할인된 가격
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    //계산하는 로직 추가
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
