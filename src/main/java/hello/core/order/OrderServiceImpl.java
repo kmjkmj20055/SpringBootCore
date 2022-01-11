cc8f95fd6c01f707cd35787f53cbc3cb5dd790fd
김민정 <kmjkmj20055@gmail.com>
2022-01-11 22:31:41 +0900

    새로운 할인 정책 적용과 문제점
    - 현재 DiscountPolicy에 할당된 값이 없어서 NPE 발생중

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -11,6 +9,7 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  //DiscountPolicy가 FixDiscountPolicy를 의존함 -> DIP 위반이므로 삭제
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
