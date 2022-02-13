86b5c9f29bd0355fab95f823ff86b1fa4756dc70
김민정 <kmjkmj20055@gmail.com>
2022-02-14 00:42:16 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -6,7 +6,6 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.pl;
 
 public class OrderApp {
 
@@ -21,7 +20,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"itemA", 10000);
+        Order order = orderService.createOrder(memberId,"itemA", 20000);
 
         System.out.println("order = " + order);
 
