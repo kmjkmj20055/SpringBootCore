9b8a7adb7c4ef8c43de9ab0d9f4890cfabf33bef
김민정 <kmjkmj20055@gmail.com>
2022-03-30 11:31:43 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(  // 스프링빈을 긁어서 끌고오는거 // 쭉 긇어오는데 스프링빈으로 등록안할 것을(여기선 Configuration.class) 지정해주는 것
+//        basePackages = "hello.core.member",  // hello.core.member 하위 디렉터리만 빈등록하도록 설정
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
